1e63eea4018dde81018fbaf502cfce738ac1297b
Magzhan <codebuster@bk.ru>
2022-03-26 17:11:55 +0600

    asdasd

--- src/pages/ParticleBg.jsx
+++ src/pages/ParticleBg.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import Particles from "react-tsparticles";
 import ParticlesConfig  from "../config/particle-config"
-import styled from 'styled-components'
-
-const Box = styled.div`
-position: absolute;
-top:0;
-right:0;
-left:0;
-bottom:0;
-z-index:0;
-`
 
 const ParticleBg = () => {
     return (
-        <Box>
+        <div className="absolute z-0">
             <Particles style={{position:'absolute',top:0}} params={ParticlesConfig}/>
-        </Box>
+        </div>
     );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import { babel } from "@rollup/plugin-babel";
 
 export default defineConfig({
   plugins: [
-    reactRefresh(),
     babel({
       include: ["./src/**"],
       extensions: [".js", ".jsx"],
